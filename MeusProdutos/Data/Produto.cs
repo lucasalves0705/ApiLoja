7c967b7bacbd62c314862ac29d5efc8dce7b44ef
lucasalves0705 <lucasalves0705@gmail.com>
2021-02-07 17:37:03 -0300

    Criação do projeto e configurações basicas

---
+++ MeusProdutos/Data/Produto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MeusProdutos.Data
+{
+    public class Produto
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+        public float valor { get; set; }
+        public bool status { get; set; }
+    }
+}
---
+++ MeusProdutos/Migrations/20210207191517_Inicial.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MeusProdutos.Migrations
+{
+    public partial class Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Produtos",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    nome = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true),
+                    valor = table.Column<float>(type: "float", nullable: false),
+                    status = table.Column<bool>(type: "tinyint(1)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produtos", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Produtos");
+        }
+    }
+}
